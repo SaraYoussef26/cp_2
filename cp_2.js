815ab3531d759e534dc47708f0f816476807b40d
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-10-16 09:03:56 -0400

    displaying the first 5 product

--- cp_2.js
+++ cp_2.js
@@ -32,7 +32,35 @@ async function fetchProductsAsync() {
             handleError(error);
         }
 }
-    
 
-        
+//Step 5
+function displayProducts(products){
+    const container = document.querySelector("#product-container");
+    container.innerHTML=" ";
+    const firstFiveProducts = products.slice(0,5);
+    if (firstFiveProducts.length===0) {
+        container.innerHTML='<div class="loading">No products found.</div>';
+        return; 
+    }
+    firstFiveProducts.forEach(product=> {
+        const productCard = createProductCard(product);
+        container.appendChild(productCard);
+    });
+    }
+
+
+function createProductCard(product) {
+    const card = document.createElement("div"); 
+    card.className= "product-card";
+    const{name, price, image}= product.fields;
+    const imageURL = image [0].url;
+    card.innerHTML =`
+    <img src="${imageUrl}" alt="${name}" class="product-image">
+        <div class="product-info">
+            <h3 class="product-name">${name}</h3>
+            <p class="product-price">$${(price / 100).toFixed(2)}</p>
+        </div>
+    `;
     
+return card;
+}
