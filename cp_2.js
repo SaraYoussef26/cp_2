7e727f2f05195b5ffa08e5efc87ad0186d48b68b
Sara Youssef <https://github.com/SaraYoussef26/Portfolio-site.git>
2025-10-16 09:41:11 -0400

    fixed syntax error

--- cp_2.js
+++ cp_2.js
@@ -2,7 +2,7 @@ function fetchProductsThen() {}
     fetch('https://www.course-api.com/javascript-store-products')
     .then(response => {
         if (!response.ok) {
-            throw new Error ('HTTP error! status: ${response.status}');
+            throw new Error (`HTTP error! status: ${response.status}`);
         }
         return response.json();
     })
@@ -24,7 +24,7 @@ async function fetchProductsAsync() {
         try {
             const response = await fetch('https://www.course-api.com/javascript-store-products');
          if (!response.ok) {
-            throw new Error ('HTTP error! status: ${response.status}');
+            throw new Error (`HTTP error! status: ${response.status}`);
         }
         const products = await response.json ();
         displayProducts(product);
